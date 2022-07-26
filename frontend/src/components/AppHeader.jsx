0694ce81cca46e54d7a103b5059000f5bb2f6a15
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-07-26 19:51:51 +0200

    feat: add langSwitcher

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,25 +1,56 @@
-import { useTranslation } from 'react-i18next';
+import { getI18n, useTranslation } from 'react-i18next';
 import { useContext } from 'react';
-import { Button, Navbar } from 'react-bootstrap';
+import { Button, ButtonGroup, Nav, Navbar } from 'react-bootstrap';
 import AuthContext from '../contexts/AuthContext';
+
+const LangSwitcher = () => {
+  const i18n = getI18n();
+
+  const handleClick = (lang) => () => i18n.changeLanguage(lang);
+
+  return (
+    <ButtonGroup role="radio" className="me-auto">
+      <Button
+        size="sm"
+        variant="outline-secondary"
+        onClick={handleClick('ru')}
+        active={i18n.language === 'ru'}
+      >
+        ru
+      </Button>
+      <Button
+        size="sm"
+        variant="outline-secondary"
+        onClick={handleClick('en')}
+        active={i18n.language === 'en'}
+      >
+        en
+      </Button>
+    </ButtonGroup>
+  );
+};
 
 const AppHeader = () => {
   const { t } = useTranslation();
-  const { isSignedIn, signOut } = useContext(AuthContext);
+  const { user, isSignedIn, signOut } = useContext(AuthContext);
 
   return (
-    <>
-      <Navbar expand="lg" className="shadow-sm bg-white">
-        <div className="container">
-          <Navbar.Brand href="/">{t('app.title')}</Navbar.Brand>
-          {isSignedIn() && (
-            <Button variant="outline-dark" onClick={signOut}>
+    <Navbar expand="lg" className="shadow-sm bg-white">
+      <div className="container">
+        <Navbar.Brand className="me-3" href="/">
+          {t('app.title')}
+        </Navbar.Brand>
+        <LangSwitcher />
+        {isSignedIn() && (
+          <>
+            <Navbar.Text className="me-3">{user?.username}</Navbar.Text>
+            <Button variant="primary" onClick={signOut}>
               {t('auth.signOut')}
             </Button>
-          )}
-        </div>
-      </Navbar>
-    </>
+          </>
+        )}
+      </div>
+    </Navbar>
   );
 };
 export default AppHeader;
